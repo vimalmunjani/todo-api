820a62e06c6681e781510819ac47d4d0acd1229a
Vimal Munjani <vimal.j.munjani@gmail.com>
2018-10-24 15:36:19 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.use('/todo',todoRouter);
+router.use('api/todo',todoRouter);
 
 module.exports = router;
